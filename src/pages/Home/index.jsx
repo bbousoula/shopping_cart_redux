1bebe96247c2e6c0017cc2a2ea28c6fd0cb0bdeb
bbousoula <89743459+bbousoula@users.noreply.github.com>
2022-02-12 21:48:20 +0400

    cart lenght

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ import { useStoreActions, useStoreState } from "easy-peasy";
 const Home = (props) => {
   const products = useStoreState((state) => state.items);
   const addProduct = useStoreActions((actions) => actions.addProduct);
+  const cartItems = useStoreState((state) => state.cartItems);
   // const addToBasket = (e) => {
   //   console.log("test");
   // };
@@ -17,6 +18,7 @@ const Home = (props) => {
             alt="basket"
             src="https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-shopping-cart-cyber-monday-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"
           />
+          {cartItems && !cartItems.length ? "" : cartItems.length}
         </Link>
       </div>
       <div className="product-container">
